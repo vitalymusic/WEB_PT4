9117958adb1d73b5fe7e86c75e4a647fd1e8d465
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-09-17 18:38:05 +0300

    DOM

--- DOM.js
+++ DOM.js
@@ -9,10 +9,34 @@ const heading1 = document.querySelector('h1');
 const heading2 = document.querySelector('#second');
 const textinDiv = document.querySelector('div p');
 const div1 = document.querySelector('div');
-
+const tabula = document.querySelector('table');
 
 const images = document.querySelectorAll('img');
 
+// Vecās metodes
+// getElementById(), getElementsByTagName(),getElementsByClassName()
+
+// 2. Satura maiņa
+
+// innerHTML innerText
+
+heading1.innerText = `Šis teksts ir izmainīts`;
+
+div1.innerHTML += "<input type=\"text\" name=\"textInp\">";
+
+// alert(heading1.innerText);
+
+let html = "";
+
+for(let i=0;i<10;i++){
+    html+=`<tr>`;
+        for(let j=0;j<10;j++){
+             html+=`<td>${i+1} un ${j+1}</td>`
+        }
+    html+=`</tr>`;
+}
+
+tabula.innerHTML += html;
 
 console.log(heading1,textinDiv,images);
 
