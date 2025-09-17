c6cd2aefb687459d0ddc300012dad4b46abb0b74
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-09-17 18:17:57 +0300

    DOm1

---
+++ DOM.js
@@ -0,0 +1,19 @@
+// Document Object Model
+
+// document.
+
+
+// 1. Elementu atlase
+
+const heading1 = document.querySelector('h1');
+const heading2 = document.querySelector('#second');
+const textinDiv = document.querySelector('div p');
+const div1 = document.querySelector('div');
+
+
+const images = document.querySelectorAll('img');
+
+
+console.log(heading1,textinDiv,images);
+
+
