f38789a2bc7dfaa7d29069cf7ef3e9fe358293d9
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-09-23 18:54:21 +0300

    Fetch1

--- js_vaicajumi.js
+++ js_vaicajumi.js
@@ -2,10 +2,31 @@
 
 // FETCH
 // XMLHttpRequest
+    document.body.style.background = "#fff";
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+
+fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => showData(json));
+
+
+
+  function showData(data){
+
+        for( post of data){
+            document.querySelector(".posts").innerHTML +=`
+                <div class="post">
+                    <h1>${post.title}</h1>
+                    <p>
+                        ${post.body}
+                    </p>
+                </div>
+            `
+
+        }
+
+  }
+
 
 
 
