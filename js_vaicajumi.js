ad741f6bbacb04a2518030b1578b04558bc9432b
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-09-23 19:54:13 +0300

    Dialog

--- js_vaicajumi.js
+++ js_vaicajumi.js
@@ -2,23 +2,20 @@
 
 // FETCH
 // XMLHttpRequest
-    document.body.style.background = "#fff";
+document.body.style.background = "#fff";
 
-    function loadData(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then((response) => response.json())
-            .then((json) => showData(json));
-    }
-
-
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => showData(json));
+}
 
 
+function showData(data) {
+    document.querySelector(".posts").innerHTML = "";
 
-  function showData(data){
-    document.querySelector(".posts").innerHTML ="";
-
-        for( post of data){
-            document.querySelector(".posts").innerHTML +=`
+    for (post of data) {
+        document.querySelector(".posts").innerHTML += `
                 <div class="post" data-userId = "${post.userId}" style="cursor:pointer" onclick = "showUser(${post.userId})">
                     <h1 >${post.title}</h1>
                     <p>
@@ -26,17 +23,40 @@
                     </p>
                 </div>
             `
+    }
+}
+
+function showUser(userID) {
+       
+            fetch('https://jsonplaceholder.typicode.com/users/'+userID)
+            .then((response) => response.json())
+            .then((json) => {
+                table = document.querySelector('.userData');
+                table.innerHTML+=`
+                <tr class="data">
+                    <td>${json.name}</td>
+                    <td>${json.email}</td>
+                    <td>${json.phone}</td>
+                </tr>    
+                `;
+
+                document.querySelector('#openDialog2').showModal();
+            });
 
             
-        }
-
-  }
-
-  function showUser(userID){
-    console.log(userID);
 
 
-  } 
+}
+
+function closeModal(element){
+
+        document.querySelector(element).close();
+        document.querySelector(".data").remove();
+
+}
+
+
+
 
 
 
